efe8e00487f792f68f170ea8dd7f45c0dbb9b1e2
Sony Åhr <68597330+xsonyhu@users.noreply.github.com>
2021-04-11 01:20:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,47 @@
 import subprocess
 import os
+import time
 
-subprocess.call(['pip', 'install', 'telepot'])
-subprocess.call(['pip', 'install', 'requests'])
-os.system('cls||clear')
+try:
+    file = open('api.txt')
+except IOError as e:
 
-print(""" ____  _____ __  __  ___ _____ _____
+	subprocess.call(['pip', 'install', 'telepot'])
+	subprocess.call(['pip', 'install', 'request'])
+	os.system('cls||clear')
+
+	print(""" ____  _____ __  __  ___ _____ _____
 |  _ \| ____|  \/  |/ _ \_   _| ____|_ __  _   _
 | |_) |  _| | |\/| | | | || | |  _| | '_ \| | | |
 |  _ <| |___| |  | | |_| || | | |___| |_) | |_| |
 |_| \_\_____|_|  |_|\___/ |_| |_____| .__/ \__, |
                                     |_|    |___/""")
 
-s = input("Enter API key: ")
-l = input("Enter your telegram id: ")
+	s = input("Enter API key: ")
+	l = input("Enter your telegram id: ")
+	os.system('cls||clear')
 
-f = open('api.txt', 'w')
-for index in s:
-    f.write(index)
-f.close()
+	f = open('api.txt', 'w')
+	for index in s:
+			f.write(index)
+	f.close()
 
-f = open('id.txt', 'w')
-for index in l:
-    f.write(index)
-f.close()
+	f = open('id.txt', 'w')
+	for index in l:
+    		f.write(index)
+	f.close()
 
-subprocess.call(['python', 'remote.py'])
+	subprocess.call(['python', 'remote.py'])  
+
+else:
+    with file:
+    	text = "you have already installed!"
+    	print(text.upper())
+    	time.sleep(1.5)
+    	os.system('cls||clear')
+    	subprocess.call(['python', 'remote.py'])
 
 
+	
+
+ 
